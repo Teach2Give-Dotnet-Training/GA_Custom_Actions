78e9808ecc8e23ce55d37fc77a86fe3662ececd3
Jonathan Ndambuki <joendambuki16@gmail.com>
2024-09-24 16:16:07 +0300

    Javascript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,7 +9,7 @@ async function run(){
 
 
     const s3Uri= `s3://${bucket}`
-    exec.exec (` aws s3 sysnc ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    exec.exec (` aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
     const websiteUrl= `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
     core.setOutput('website-url', websiteUrl)
